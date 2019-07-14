9545160ae59ac58437c10bdd885e061bb2ad7b3f
kilometers <kilo@meters.sh>
2019-07-14 18:55:32 +0000

    Fix bracket

--- bot.sh
+++ bot.sh
@@ -2,7 +2,7 @@
 tail -fn0 "/var/log/auth.log" | \
 while read LINE ; do
     MESSAGE=$(echo $LINE | awk '/Accepted publickey/ { print "New login to user " $9 " from IP " $11 "\n" $15 " " $16 }')
-    if [ -n "${MESSAGE" ]; then # test if $MESSAGE is empty
+    if [ -n "${MESSAGE}" ]; then # test if $MESSAGE is empty
         curl -X POST \
              -H 'Content-Type: application/json' \
              -d "{\"chat_id\": \"${CHAT_ID}\", \"text\": \"${MESSAGE}\"}" \
